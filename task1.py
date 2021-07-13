57bc884a5696663698f9a06faff2ee3a396a87af
ozim73 <86981781+ozim73@users.noreply.github.com>
2021-07-13 16:17:49 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,37 +1,54 @@
 import re
 
-#Ввод символів
+# Ввод символів
 row=input("\nВведіть слова та числа: ")
+# Виведення введених символів
+print("\nВведені символи: ", row)
 
-#Виведення введених символів
-print("\nВведені символи ", row)
 
-extract_letter=" ".join(re.split("[0-9]+", row))
+extract_letter="".join(re.split("[0-9]+", row))
 extract_number="".join(re.split("[a-zA-Z]+", row))
- 
+
+
 extract_com_let=", ".join(extract_letter.split())
 extract_com_num=", ".join(extract_number.split())
 
-# Виведення введених чисел
-print("\nВсі числа: ", str(extract_com_num))
-
-#Виведення введених слів
-print("\nВсі слова ", str(extract_com_let))
+print("\nВведені слова: ", str(extract_com_let))
 
 
-
+row_new_list=[]
 for let in extract_letter.split():
-    print("\nВиведення слів з першими та останніми великими буквами ", let[0].upper() + let[1:-1].lower() + let[-1].upper(), end="\n")
+    row_new_list.append(let[0].upper() + let[1:-1].lower() + let[-1].upper())
+upper_let=" ".join(row_new_list)
+print("\nВиведення слів з першими та останніми великими буквами: ", upper_let) 
 
-new_num_max=list(map(int, extract_number.split()))
 
-max_num=max(new_num_max)
+max_num=""
+numb_new_string=""
 
-# Функція заходження та виведення максимального значення
-print("\nМаксимальне значення ", max_num)
-for element in new_num_max:
-    if element != max_num:
-        element=element**new_num_max.index(element)
-        print("\nЧисла піднесені до кореня їх індексу ", element)
-    else:
-        continue
+ 
+for numbers in extract_number:
+
+    if "0" <= numbers <= "9":
+    
+
+        new_num_max=list(map(int, extract_number.split()))
+
+# Виведення максимального значення
+        max_num=max(new_num_max)
+
+
+        numb_new_list=[]
+        for element in new_num_max:
+            if element != max_num:
+                element=(element**new_num_max.index(element))
+                numb_new_list.append(element)
+            else:
+                continue
+
+        numb_new_string=", ".join([str(i) for i in numb_new_list])
+
+# Виведення чисел
+print("\nВсі числа: ", str(extract_com_num))
+print("\nМаксимальне значення: ", max_num)
+print("\nЧисла піднесені до кореня їх індексу: ", numb_new_string)
